4ac802a56cb32a3b10e2ad6b94193896ab89dd68
ahmed <ea_labed@esi.dz>
2021-03-22 08:59:12 +0100

    Style commit

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -1,4 +1,6 @@
 import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
+
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -9,15 +11,19 @@ function ShoppingList() {
 
 	return (
 		<div>
-			<ul>
+             <ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
-				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}</li>
-                    
+
+			<ul className='lmj-plant-list'>
+            {plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+                        {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+
+					</li>
 				))}
 			</ul>
             
